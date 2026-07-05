f702823e29ed65f74d285eedbf5abffa949f58f5
Simarkaur03 <ksimar232@gmail.com>
2026-07-06 03:26:50 +0530

    deleted duplicate folder and edited tsconfig to not show compatibility error

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -6,4 +6,30 @@ if (!process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN) {
   throw new Error("NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN missing");
 }
 
-// ...repeat for the remaining variables
+if (!process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_PROJECT_ID missing");
+}
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET missing");
+}
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID missing");
+}
+
+if (!process.env.NEXT_PUBLIC_FIREBASE_APP_ID) {
+  throw new Error("NEXT_PUBLIC_FIREBASE_APP_ID missing");
+}
+
+export const env = {
+  firebase: {
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:
+      process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  },
+};
